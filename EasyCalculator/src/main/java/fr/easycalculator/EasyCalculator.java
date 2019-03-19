47c626d45ba2e424682651126c0f0a96c1bd502d
André <sustac.andre@gmail.com>
2019-03-19 15:05:06 +0200

    Creating project

---
+++ EasyCalculator/src/main/java/fr/easycalculator/EasyCalculator.java
@@ -0,0 +1,13 @@
+package fr.easycalculator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EasyCalculator extends JavaPlugin {
+	
+	@Override
+    public void onEnable() { }
+	
+	@Override
+    public void onDisable() {  }
+	
+}
